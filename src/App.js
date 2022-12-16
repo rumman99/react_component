410b30a01f861c661ecae09ef5187406f7ae9afc
rumman99 <tasnimrumman369@gmail.com>
2022-12-17 04:45:59 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { click } from '@testing-library/user-event/dist/click';
 
 function App() {
-  let tar=['roshogolla', "dudh", "chana"];
-  let pc_game= ["valorant", "COD", "Dota2"];
+  let tar=['roshogolla', "chana", 'pizza', 'kacchi'];
+  // let pc_game= ["valorant", "COD", "Dota2"];
   const product= [
     {product_name: 'Game 1', game: 'Valorant', price: '$500'},
     {product_name: 'Game 2', game: 'Dota 2', price: '$800'},
     {product_name: 'Game 3', game: 'Pubg', price: '$1000'}
   ]
-
+  const gg= tar.map(s => s);
+  console.log(gg);
   return (
     <div className="App">
       <header className="App-header">
+        <Increase></Increase>
         {/* <h1>Ridhan</h1>
         <Game food="kacchi" sport="football"></Game>
         <Game food= "biriyani" sport="cricket"></Game>
         <Game food={tar[0]} sport={pc_game[0]}></Game>
         <Game food={tar[2]} sport={pc_game[1]}></Game>
         <Game food={tar[1]} sport={pc_game[2]}></Game> */}
-        <Box tag= {product[0]} name={product[0].game} price0={product[0]}></Box>
+
+        {/* <Box tag= {product[0]} name={product[0].game} price0={product[0]}></Box>
         <Box tag= {product[1]} name={product[1].game} price0={product[1]}></Box>
-        <Box tag= {product[2]} name={product[2].game} price0={product[2]}></Box>
+        <Box tag= {product[2]} name={product[2].game} price0={product[2]}></Box> */}
+
+{/*//////////////////////Non Dynamic Element/////////////////// */}
+        <Box  products={product[0]}></Box>
+        <Box  products={product[1]}></Box>
+        <Box  products={product[2]}></Box>
+
+{/*////////////////////////////Dynamic Element//////////////////////////////////*/}
+        {
+          product.map(ez => <Box products= {ez}></Box>)
+        }
+        <ul>
+            {
+            tar.map(gg => <li>{gg}</li>)              
+            }
+            {
+              product.map(wp => <li>{wp.price}-{wp.game}</li>)
+            }
+        </ul>
       </header>
     </div>
   );
@@ -58,15 +81,39 @@ function Box(props){
     backgroundColor: 'green',
     color: 'white'
   }
-    const {product_name, game, price} = props.tag;
+    const {product_name, game, price} = props.products;
     return (
       <div style={design}>
           <h3> {product_name}</h3>
-          <h4>{props.name}</h4>
-          <h2 style={{color:"red"}}>{props.price0.price}</h2>
+          <h4>{game}</h4>
+          <h2 style={{color:"red"}}>{price}</h2>
           <button style={btn}>Buy Now</button>
       </div>
     )
 }
+
+
+/*//////////////////////////////////////*/
+function Increase(){
+const [count, setCount] = useState(0);
+let plus, minus;
+
+if(count <20){
+plus = () => setCount(count+1);
+}
+if(count >0){
+minus = () => setCount(count-1);
+}
+
+
+  return (
+    <div>
+    <h2>Number: {count} </h2>
+    <button onClick={plus} style={{padding: '10px'}}>+</button>
+    <button onClick={minus} style={{padding: '10px'}}>-</button>
+    </div>
+  )
+}
+
 
 export default App;
